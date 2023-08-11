792ee7e756889f3fd917b5d703f458d872a0022b
ahmadilham_13 <ahmad13ilham13@gmail.com>
2023-08-11 15:09:09 +0700

    change title

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
       >
         <div className="mr-auto place-self-center lg:col-span-7">
           <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-            Payments tool for software companies
+            Payments tool for software companies DEV
           </h1>
           <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
             From checkout to global sales tax compliance, companies around the
