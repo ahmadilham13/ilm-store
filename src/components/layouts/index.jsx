680ef21eebd35deda76697fa0bcf506b72ebc68a
ahmadilham_13 <ahmad13ilham13@gmail.com>
2023-08-11 15:54:57 +0700

    update: component

--- src/components/layouts/index.jsx
+++ src/components/layouts/index.jsx
@@ -1,5 +1,13 @@
+import Head from "next/head";
 import Main from "./main";
 
 export default function Layout({ children }) {
-  return <Main>{children}</Main>;
+  return (
+    <>
+      <Head>
+        <title>ILM Store</title>
+      </Head>
+      <Main>{children}</Main>
+    </>
+  );
 }
