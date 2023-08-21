84f28cd7d55bbc280663827aba551966549bd491
ahmadilham_13 <ahmad13ilham13@gmail.com>
2023-08-21 08:37:25 +0700

    fix: navbar

--- src/components/layouts/navbar.jsx
+++ src/components/layouts/navbar.jsx
@@ -1,4 +1,3 @@
-import { Navbar } from "flowbite-react";
 import { useState } from "react";
 import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 import { AiOutlineShoppingCart } from "react-icons/ai";
@@ -87,7 +86,8 @@ export default function NavbarDefault() {
             <li>
               <Link
                 href="/products"
-                className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                // className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 aria-current="page"
               >
                 Products
